b55a288a003984768ec56a51b2f02e89af64ce33
João Correia <jlmc@ic.ufal.br>
2019-07-04 15:02:52 -0300

    Add new log output and images

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -4,22 +4,29 @@ import numpy as np
 logging.basicConfig(level=logging.DEBUG)
 
 class Adaline(object):
-
+    
     def __init__(self, eta=0.01, epochs=50):
         self.eta = eta
         self.epochs = epochs
 
     def train(self, X, y):
+        logging.info("STARTING TRAINING ...")
 
         self.w_ = np.zeros(1 + X.shape[1])
-        self.cost_ = []
+        logging.info("	W: {}".format(self.w_))
 
+        self.cost_ = []
         for i in range(self.epochs):
+            logging.info("  EPOCH: {}".format(i))
+            
             output = self.net_input(X)
             errors = (y - output)
             self.w_[1:] += self.eta * X.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
             cost = (errors**2).sum() / 2.0
+
+            logging.info("	W: {}".format(self.w_))
+            logging.info("      COST: {}".format(cost))
             self.cost_.append(cost)
         return self
 
